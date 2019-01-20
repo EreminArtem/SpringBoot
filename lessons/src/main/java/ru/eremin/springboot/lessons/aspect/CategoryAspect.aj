415284aa38b5410f07dd49708e4e7222c3c3dc27
EreminArtem <Eremin-artem@inbox.ru>
2019-01-20 17:39:35 +0300

    2

--- lessons/src/main/java/ru/eremin/springboot/lessons/aspect/CategoryAspect.aj
+++ lessons/src/main/java/ru/eremin/springboot/lessons/aspect/CategoryAspect.aj
@@ -4,5 +4,12 @@ package ru.eremin.springboot.lessons.aspect;
  @autor Eremin Artem on 20.01.2019.
  */
 
-public aspect CategoryAspect {
+aspect CategoryAspect {
+
+    pointcut publicMethodCall():
+            call(public * ru.eremin.springboot.lessons.repository.CategoryRepository.*.*(..));
+
+    before () : publicMethodCall(){
+        System.out.println(thisJoinPoint);
+    }
 }
--- lessons/src/main/java/ru/eremin/springboot/lessons/aspect/NoteAspect.java
+++ lessons/src/main/java/ru/eremin/springboot/lessons/aspect/NoteAspect.java
@@ -1,8 +1,35 @@
 package ru.eremin.springboot.lessons.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
 /**
  * @autor Eremin Artem on 20.01.2019.
  */
 
+@Aspect
+@Component
 public class NoteAspect {
+    @Pointcut("execution(* ru.eremin.springboot.lessons.repository.NoteRepository.*(..))")
+    public void getName() {
+
+    }
+
+    @Before("getName()")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println(joinPoint);
+    }
+
+    @Around("getName()")
+    public Object monitor(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        final long time = System.nanoTime();
+        final Object object = proceedingJoinPoint.proceed();
+        System.out.println(System.nanoTime() - time + " Method work time");
+        return object;
+    }
 }
